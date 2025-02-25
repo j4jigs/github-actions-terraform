f482f815674471541b919c03d1789969d12da6a6
j4jigs <j4jigs@gmail.com>
2025-02-25 15:13:28 +0530

    updated bucketname

--- terraform/vpc/environment/dev/main.tf
+++ terraform/vpc/environment/dev/main.tf
@@ -8,7 +8,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "cloudgeeks-backend1jignesh"
+    bucket = "cloudgeeks-backendforterraform"
     key = "cloudgeeks.tfstate"
     region = "us-east-1"
   }
